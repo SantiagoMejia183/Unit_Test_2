df710656e9eec247c98d835dc68358bebdca74cf
SantiagoMejia183 <32145854+SantiagoMejia183@users.noreply.github.com>
2019-10-21 04:26:37 -0400

    Create flask_unit_test.py

---
+++ flask_unit_test.py
@@ -0,0 +1,33 @@
+import unittest
+
+from flask_python import app
+
+class testFlask(unittest.TestCase):
+    def test_distance_response(self):
+        c = app.test_client()
+        response = c.get('/distance/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_retirement_response(self):
+        c = app.test_client()
+        response = c.get('/retirement/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_retirement_data(self):
+        c = app.test_client()
+        response = c.get('/retirement/')
+        if response.data:
+            print('true')
+        elif response.data:
+            print('false')
+
+    def test_distance_data(self):
+        c = app.test_client()
+        response = c.get('/distance/')
+        if response.data:
+            print('true')
+        elif response.data:
+            print('false')
+
+if __name__ == "__main__":
+  unittest.main()
