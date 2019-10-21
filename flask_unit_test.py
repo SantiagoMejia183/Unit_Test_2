798ceda95dcad035032c0780388d80385de6a977
SantiagoMejia183 <32145854+SantiagoMejia183@users.noreply.github.com>
2019-10-21 11:48:03 -0400

    Update flask_unit_test.py

--- flask_unit_test.py
+++ flask_unit_test.py
@@ -1,33 +1,38 @@
 import unittest
+from DBTest import DBTest
 
 from flask_python import app
 
 class testFlask(unittest.TestCase):
     def test_distance_response(self):
+        openOrNot = DBTest.open_connection(self)
         c = app.test_client()
         response = c.get('/distance/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(openOrNot)
 
     def test_retirement_response(self):
+        openOrNot = DBTest.open_connection(self)
         c = app.test_client()
         response = c.get('/retirement/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(openOrNot)
 
     def test_retirement_data(self):
+        dataAvl = DBTest.store_data(self)
         c = app.test_client()
         response = c.get('/retirement/')
         if response.data:
-            print('true')
+            dataAvl
         else:
-            print('false')
+            dataAvl
 
     def test_distance_data(self):
+        dataAvl = DBTest.store_data(self)
         c = app.test_client()
         response = c.get('/distance/')
         if response.data:
-            print('true')
+            dataAvl
         else:
-            print('false')
+            dataAvl
 
 if __name__ == "__main__":
   unittest.main()
